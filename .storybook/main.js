f4942c417e5f0949fea4de0e4a4f11aa6a0232ea
Mateusz Skwierczyński <mskwierczynski@apptension.com>
2021-12-04 15:32:35 +0100

    fix: fix tailwind storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,16 @@
 module.exports = {
   stories: ['../lib/**/*.stories.mdx', '../lib/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
   framework: '@storybook/react',
 };
