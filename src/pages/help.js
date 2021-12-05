e673ad9a4c95f92d9081fb5f3bd39f3856082739
Mateusz Skwierczyński <mskwierczynski@apptension.com>
2021-12-05 19:52:25 +0100

    Fix md format

--- src/pages/help.js
+++ src/pages/help.js
@@ -5,14 +5,15 @@ import { MDXRemote } from 'next-mdx-remote';
 import { Layout } from '../components/layout';
 
 const components = {
-  h1: ({ children }) => <h1 className="text-xxl font-bold">{children}</h1>,
-  h2: ({ children }) => <h1 className="text-xl font-bold">{children}</h1>,
+  h1: ({ children }) => <h1 className="text-2xl font-bold">{children}</h1>,
+  h2: ({ children }) => <h2 className="text-xl font-bold">{children}</h2>,
+  p: ({ children }) => <p className="mb-2">{children}</p>,
 };
 
 export default function Help({ source }) {
   return (
     <Layout>
-      <div className="max-w-lg w-full m-auto mt-4">
+      <div className="max-w-7xl w-full m-auto mt-6 mb-6">
         <MDXRemote {...source} components={components} />
       </div>
     </Layout>
