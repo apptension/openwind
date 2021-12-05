b791aee042347176a5469b979c831ef997fb1b35
Kamil <kamil.kedzierski@pluscode.pl>
2021-12-05 21:57:57 +0100

    Added new nav item

--- src/components/navbar/navbar.component.js
+++ src/components/navbar/navbar.component.js
@@ -32,29 +32,31 @@ export function NavbarComponent() {
                 </a>
               </Link>
             ))}
-            <a
-              href="https://github.com/apptension/openwind/issues/new?title=[element]%20Insert%20the%20title%20and%20leave%20the%20[element]%20tag%20at%20the%20beginning!"
-              target="_blank"
-              rel="noreferrer"
-              className="bg-transparent text-black hover:bg-gray-300 hover:text-gray-700 transition-colors inline-flex items-center px-2 py-1 rounded text-sm font-medium"
-            >
-              Request a component
-            </a>
           </div>
         </div>
-        <a href="https://github.com/apptension/openwind" target="_blank" rel="noreferrer noopener">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="black"
+        <div className="flex items-center">
+          <a
+            href="https://github.com/apptension/openwind/issues/new?title=[element]%20Insert%20the%20title%20and%20leave%20the%20[element]%20tag%20at%20the%20beginning!&body=Remember%20to%20insert%20the%20description%20!%20%F0%9F%A4%93"
+            target="_blank"
+            rel="noreferrer"
+            className="bg-transparent text-black hover:bg-gray-300 hover:text-gray-700 transition-colors inline-flex items-center px-2 py-1 rounded text-sm font-medium mr-2"
           >
-            <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"></path>
-          </svg>
-        </a>
+            Request a component
+          </a>
+          <a href="https://github.com/apptension/openwind" target="_blank" rel="noreferrer noopener">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="black"
+            >
+              <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z"></path>
+            </svg>
+          </a>
+        </div>
       </div>
     </nav>
   );
